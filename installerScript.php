a5518fe1b8590a7a5bf6d5cd6efe54a244abcc12
ReLater <ReLater@users.noreply.github.com>
2019-02-11 00:35:19 +0100

    installerScript.php for minimumPHP check

---
+++ installerScript.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Use in your extension manifest file (any tag is optional!!!!!):
+ * <minimumPhp>7.0</minimumPhp>
+ * <minimumJoomla>3.9</minimumJoomla>
+ * Yes, use 999999 to match '3.9'. Otherwise comparison will fail.
+ * <maximumJoomla>3.9.999999</maximumJoomla>
+ * <maximumPhp>7.3.999999</maximumPhp>
+ * <allowDowngrades>1</allowDowngrades>
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Installer\InstallerScript;
+
+class plgSystemCharacterscounterGhsvsInstallerScript extends InstallerScript
+{
+	public function preflight($type, $parent)
+	{
+		$manifest = @$parent->get('manifest');
+		
+		if ($manifest instanceof SimpleXMLElement)
+		{
+			$minimumPhp = trim((string) $manifest->minimumPhp);
+			$minimumJoomla = trim((string) $manifest->minimumJoomla);
+			
+			// Custom
+			$maximumPhp = trim((string) $manifest->maximumPhp);
+			$maximumJoomla = trim((string) $manifest->maximumJoomla);
+			
+			$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
+			$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
+
+			if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+			{
+				$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
+				JLog::add($msg, JLog::WARNING, 'jerror');
+			}
+
+			// Check for the maximum PHP version before continuing
+			if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
+			{
+				$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+				
+				JLog::add($msg, JLog::WARNING, 'jerror');
+			}
+			
+			if (isset($msg))
+			{
+				return false;
+			}
+			
+			if (trim((string) $manifest->allowDowngrades))
+			{
+				$this->allowDowngrades = true;
+			}
+		}
+		return true;
+	}
+}
