1829fe86fe436d26668ca766808bd4063501b6c3
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-08-11 20:53:44 +0200

    Add checksum sha256

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const util = require("util");
 const rimRaf = util.promisify(require("rimraf"));
 const chalk = require('chalk');
 const replaceXml = require('./build/replaceXml.js');
+const crypto = require('crypto');
 
 const {
 	name,
@@ -21,6 +22,27 @@ async function cleanOut (cleanOuts) {
 			answer => console.log(chalk.redBright(`rimrafed: ${file}.`))
 		).catch(error => console.error('Error ' + error));
 	}
+}
+
+// Digest sha256, sha384 or sha512.
+async function getChecksum(path, Digest)
+{
+  return new Promise(function (resolve, reject)
+	{
+    const hash = crypto.createHash(Digest);
+    const input = fse.createReadStream(path);
+
+    input.on('error', reject);
+    input.on('data', function (chunk)
+		{
+      hash.update(chunk);
+    });
+
+    input.on('close', function ()
+		{
+      resolve(hash.digest('hex'));
+    });
+  });
 }
 
 (async function exec()
@@ -57,33 +79,50 @@ async function cleanOut (cleanOuts) {
 			`Copied ${manifestFileName} to ./dist.`))
 	);
 
+	// Create zip file and detect checksum then.
+	const zipFilePath = `./dist/${zipFilename}`;
+
+	const zip = new (require('adm-zip'))();
+	zip.addLocalFolder("package", false);
+	await zip.writeZip(`${zipFilePath}`);
+	console.log(chalk.cyanBright(chalk.bgRed(
+		`./dist/${zipFilename} written.`)));
+
+	const Digest = 'sha256'; //sha384, sha512
+	const checksum = await getChecksum(zipFilePath, Digest)
+  .then(
+		hash => {
+			const tag = `<${Digest}>${hash}</${Digest}>`;
+			console.log(chalk.greenBright(`Checksum tag is: ${tag}`));
+			return tag;
+		}
+	)
+	.catch(error => {
+		console.log(error);
+		console.log(chalk.redBright(`Error while checksum creation. I won't set one!`));
+		return '';
+	});
+
 	let xmlFile = 'update.xml';
 	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
 		answer => console.log(chalk.yellowBright(
 			`Copied ${xmlFile} to ./dist.`))
 	);
-	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename);
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum);
 
 	xmlFile = 'changelog.xml';
 	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
 		answer => console.log(chalk.yellowBright(
 			`Copied ${xmlFile} to ./dist.`))
 	);
-	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename);
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum);
 
 	xmlFile = 'release.txt';
 	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
 		answer => console.log(chalk.yellowBright(
 			`Copied ${xmlFile} to ./dist.`))
 	);
-	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename);
-
-	// Zip it
-	const zip = new (require("adm-zip"))();
-	zip.addLocalFolder("package", false);
-	await zip.writeZip(`./dist/${zipFilename}`);
-	console.log(chalk.cyanBright(chalk.bgRed(
-		`./dist/${zipFilename} written.`)));
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum);
 
 	cleanOuts = [
 		`./package`,
--- build/replaceXml.js
+++ build/replaceXml.js
@@ -9,6 +9,7 @@ const {
 	author,
 	update,
 	changelog,
+	releaseTxt,
 	copyright,
 	creationDate,
 	description,
@@ -25,15 +26,24 @@ const {
 	bugs
 } = require("./../package.json");
 
-module.exports.main = async (xmlFile, zipFilename) =>
+module.exports.main = async (xmlFile, zipFilename, checksum) =>
 {
   try
   {
+		let checksumEntity = '';
+		if (checksum)
+		{
+			checksumEntity = checksum.replace(/</g, '&lt;');
+			checksumEntity = checksumEntity.replace(/>/g, '&gt;');
+		}
+
 		let xml = await fse.readFile(xmlFile, { encoding: "utf8" });
 		xml = xml.replace(/{{allowDowngrades}}/g, allowDowngrades);
 		xml = xml.replace(/{{authorName}}/g, author.name);
 		xml = xml.replace(/{{authorUrl}}/g, author.url);
 		xml = xml.replace(/{{bugs}}/g, bugs);
+		xml = xml.replace(/{{checksum}}/g, checksum);
+		xml = xml.replace(/{{checksumEntity}}/g, checksumEntity);
 		xml = xml.replace(/{{client}}/g, update.client);
 		xml = xml.replace(/{{copyright}}/g, copyright);
 		xml = xml.replace(/{{creationDate}}/g, creationDate);
@@ -57,6 +67,7 @@ module.exports.main = async (xmlFile, zipFilename) =>
 		xml = xml.replace(/{{nameUpper}}/g, name.toUpperCase());
 		xml = xml.replace(/{{php_minimum}}/g, minimumPhp);
 		xml = xml.replace(/{{projecturl}}/g, changelog.projecturl);
+		xml = xml.replace(/{{releaseTxt.title}}/g, releaseTxt.title);
 		xml = xml.replace(/{{tag}}/g, update.tag);
 		xml = xml.replace(/{{targetplatform}}/g, update.targetplatform);
 		xml = xml.replace(/{{type}}/g, update.type);
